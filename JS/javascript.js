bf04a1ecbf2218f6acba7d9f51b6952d5c9b1561
Breno Leite <brenolleite@gmail.com>
2015-08-01 22:33:28 -0300

    About Me and Resume.

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,8 +1,15 @@
+lastLink = undefined
+
 $(function() {
     $('.click').click( function(event) {
 	var $this = $(this);
         event.preventDefault();
         $('#main-wrapper').load($this.attr('href'));
+	if(lastLink){
+	    lastLink.removeClass('active');
+	}
+	lastLink = $this;
+	$this.addClass('active');
 	return false;
     });
 });
