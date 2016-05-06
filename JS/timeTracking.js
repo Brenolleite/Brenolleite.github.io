0a761b4d1a58d040ff292d938373ca25f5d70062
Breno Leite <brenolleite@gmail.com>
2016-05-06 01:27:47 -0300

    Adicionando edicao de tempo

--- JS/timeTracking.js
+++ JS/timeTracking.js
@@ -1,10 +1,10 @@
-var app = angular.module('timeTracking', ['ngCookies']);
+var app = angular.module('timeTracking', ['ngCookies', 'ui.mask']);
 
 app.controller('timeTrackingCtr',['$scope', '$interval', '$window', '$timeout', '$cookies', function($scope, $interval, $window, $timeout, $cookies) {
   var timer, selectedTimer = -1, editing = false;
 
   $scope.timers = [];
-  
+
 	$scope.start = function(){
 		if(timer == undefined)
 			timer = $interval(ticking, 1000);
@@ -20,7 +20,8 @@ app.controller('timeTrackingCtr',['$scope', '$interval', '$window', '$timeout', 
 		$scope.timers.push({time: 0,
 												description: '',
 												editDesc: false,
-                        editTimer: false});
+                        editTimer: false,
+                        editedTime: undefined});
 		selectedTimer = $scope.timers.length - 1;
 		if(timer == undefined)
 			$scope.start();
@@ -49,6 +50,7 @@ app.controller('timeTrackingCtr',['$scope', '$interval', '$window', '$timeout', 
   }
 
   $scope.editTimer = function(index){
+    $scope.timers[index].editedTime = $scope.displayTime($scope.timers[index].time);
     editing = true;
 		$scope.timers[index].editTimer = true;
 		$timeout(function(){
@@ -58,6 +60,41 @@ app.controller('timeTrackingCtr',['$scope', '$interval', '$window', '$timeout', 
 		}, 10);
   }
 
+  $scope.saveTimer = function(index){
+    editing = false;
+    $scope.timers[index].editTimer = false;
+    if($scope.timers[index].editedTime != undefined && $scope.timers[index].editedTime != '000000' && $scope.timers[index].editedTime.length == 6){
+      time = $scope.timers[index].editedTime.match(/.{1,2}/g);
+
+      if(parseInt(time[1]) >= 60 || parseInt(time[2]) >= 60){
+        swal({
+			    title: "Tempo incorreto!",
+			    text: "O tempo não foi atualizado, tente novamente!",
+			    type: "warning",
+          confirmButtonText: "OK",
+          closeOnConfirm: true
+        });
+        return;
+      }
+
+
+      swal({
+			title: "Tem certeza que deseja alterar o contador para " + time[0] + ':' + time[1] + ':' + time[2] +  "? ",
+			text: "Não será possível reverter essa operação!",
+			type: "warning",
+			showCancelButton: true,
+			confirmButtonColor: "#DD6B55",
+			confirmButtonText: "Sim!",
+			cancelButtonText: "Cancelar",
+			closeOnConfirm: true}, 
+				 function(){ 				              
+           $scope.timers[index].time = parseInt(time[0]) * 3600 + parseInt(time[1]) * 60 + parseInt(time[2]);
+           $scope.$digest();
+				 });
+     
+    }
+  }
+
   $scope.leaveEdition = function(index){
 		editing = false;
 		$scope.timers[index].editDesc = false;
@@ -65,8 +102,12 @@ app.controller('timeTrackingCtr',['$scope', '$interval', '$window', '$timeout', 
   }
 
   $scope.enterAsTab = function(event, index){
-		if(event.keyCode == 13 && editing)
+		if(event.keyCode == 13 && editing){
+      if($scope.timers[index].editTimer)
+        $scope.saveTimer(index);
+
 			$scope.leaveEdition(index);
+    }
   }
 
   $scope.deleteTimer = function(index){
@@ -182,14 +223,8 @@ app.controller('timeTrackingCtr',['$scope', '$interval', '$window', '$timeout', 
 
     return null;
   }
-  
-  $window.onunload = function() {
-	  var status = $cookies.get('Timer');
-    while(status == undefined)
-      status = $cookies.get('Timer');
-  }
     
-  var backUp = function(saved){
+  var backUp = function(){
     $cookies.put('Timer', JSON.stringify($scope.timers, function(key, value){
 			if(key === "$$hashKey")
 				return undefined;			
@@ -202,6 +237,4 @@ app.controller('timeTrackingCtr',['$scope', '$interval', '$window', '$timeout', 
   if(cookieTimer != undefined){
 		$scope.timers = JSON.parse(cookieTimer);
   }
-  
-  $scope.time = 0;
 }]);
