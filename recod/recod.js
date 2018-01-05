311f18d3b4e676da57cab56144d96bc025150e4f
Breno Leite <brenolleite@gmail.com>
2018-01-05 14:05:03 -0200

    Users usage working

--- recod/recod.js
+++ recod/recod.js
@@ -7,10 +7,34 @@ recodApp.controller('recodCtr',['$scope', '$http', 'graphs',
     function users_usage(){
       $http.get('http://' + server + '/api/usersUsage?date_filter=2018-01-01')
       .success(function(response){
-        graphs.updateGraph('horizontalBar',
-                            ['teste', 'teste2'],
-                            [{label: 'ALOU', data: [10, 5]},
-                            {label: 'ALOU2', data: [10, 5]}]);
+        labels = []
+        idle = []
+        used = []
+
+        response.forEach(function(item){
+          labels.push(item.username);
+          idle.push(item.idle_time/60);
+          used.push(item.used_time/60);
+        });
+
+        datasets = [
+          {
+            label: 'Used Time (h)',
+            data: used,
+            backgroundColor: 'rgba(0, 230, 0, 0.7)',
+            borderColor: 'rgba(0, 128, 0, 0.7)',
+            borderWidth: 2
+          },
+          {
+            label: 'Idle Time (h)',
+            data: idle,
+            backgroundColor: 'rgba(255, 0, 0, 0.7)',
+            borderColor: 'rgba(153, 0, 0, 0.7)',
+            borderWidth: 2
+          }
+        ]
+
+        graphs.updateGraph('horizontalBar', labels, datasets);
       });
     }
 
@@ -19,4 +43,6 @@ recodApp.controller('recodCtr',['$scope', '$http', 'graphs',
         users_usage();
       }
     }
+
+    users_usage()
 }]);
