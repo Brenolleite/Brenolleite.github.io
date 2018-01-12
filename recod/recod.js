a5499f86d1d5e90718a0f7d9dfa182cea12319b4
Breno Leite <brenolleite@gmail.com>
2018-01-12 01:33:40 -0200

    Creating routines for filters

--- recod/recod.js
+++ recod/recod.js
@@ -1,20 +1,47 @@
 recodApp.controller('recodCtr',['$scope', '$http', 'graphs', '$interval',
   function($scope, $http, graphs, $interval) {
     // server config
-    server = 'serverdojesus.duckdns.org:6664';
-
+    var server = 'serverdojesus.duckdns.org:6664';
+    var request = ''
     $scope.halt = false;
+
+    // Roughly verify date
+    function isDate(date){
+      console.log(date);
+      var re = new RegExp('[0-9]{2}/[0-2][0-9]/[0-9]{4}');
+      return re.test(date);
+    }
+
+    function applyFilters(){
+      // Verify filterss
+      if(isDate($scope.date_filter1) && isDate($scope.date_filter2)){
+        params = '?date_filter1=' + $scope.date_filter1 +
+                 '&date_filter2=' + $scope.date_filter2;
+
+        // Call last request with new filters
+        if(request == 'usersUsage')
+          users_usage(params)
+        else
+          dls_usage(params)
+      }
+    }
 
     function sanityCheck(){
       $http.get('http://' + server + '/api/sanityCheck').success(function(response){
         $scope.halt = !response[0].sanity_check;
-        console.log(response[0].sanity_check)
       });
     }
 
     // Bars graph for users usage
-    function users_usage(){
-      $http.get('http://' + server + '/api/usersUsage')
+    function users_usage(params){
+      // Blank string intead undefined on params
+      if(params == undefined)
+        params = ''
+
+      // Store last request for filters
+      request = 'usersUsage'
+
+      $http.get('http://' + server + '/api/usersUsage' + params)
       .success(function(response){
         labels = []
         idle = []
@@ -48,7 +75,14 @@ recodApp.controller('recodCtr',['$scope', '$http', 'graphs', '$interval',
     }
 
     // Graphs for dls usage control
-    function dls_usage(){
+    function dls_usage(params){
+      // Blank string intead undefined on params
+      if(params == undefined)
+      params = ''
+
+      // Store last request for filters
+      request = 'dlsUsage'
+
       $http.get('http://' + server + '/api/dlsUsage')
       .success(function(response){
         labels = []
@@ -107,9 +141,16 @@ recodApp.controller('recodCtr',['$scope', '$http', 'graphs', '$interval',
         dls_usage();
     }
 
+    // Execute first sanity check
+    sanityCheck();
+
     // Starting graph
-    dls_usage()
+    dls_usage();
 
     // Create sanity check for server icon warning 900000
-    $interval(sanityCheck, 900000)
+    $interval(sanityCheck, 900000);
+
+    // Active watches on filters
+    $scope.$watch('date_filter1', applyFilters);
+    $scope.$watch('date_filter2', applyFilters);
 }]);
